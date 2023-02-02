595a0b828b141795776981e29f85662b6dabd7d4
patriceandrea <andrea_pavico98@yahoo.ca>
2023-02-02 13:27:00 -0500

    setups routing and new form components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'react-bootstrap';
 import { Routes, Route, Navigate, BrowserRouter } from 'react-router-dom';
-
+import NewNote from './NewNote'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/new" element={<h1>New</h1>} />
-        <Route path='*' element={<Navigate to="/" />} />
-      </Routes>
-    </BrowserRouter>
+    <Container className='my-4'>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/new" element={<NewNote />} />
+          <Route path='/:id'>
+            <Route index element={<h1>Show</h1>} />
+            <Route path='edit' element={<h1>Edit</h1>} />
+          </Route>
+          <Route path='*' element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
+    </Container>
   );
 }
 
---
+++ client/src/NewNote.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NoteForm from './Components/NoteForm';
+
+const NewNote = () => {
+  return (
+    <>
+      <h1 className='mb-4'>New Notes</h1>
+      <NoteForm />
+    </>
+  )
+}
+
+export default NewNote;
