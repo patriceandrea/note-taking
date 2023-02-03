97c17bb77828768cbde11e6de53f471995b638e1
patriceandrea <andrea_pavico98@yahoo.ca>
2023-02-03 12:17:33 -0500

    functionality to create new note

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { useMemo } from 'react';
 import { Container } from 'react-bootstrap';
 import { Routes, Route, Navigate, BrowserRouter } from 'react-router-dom';
 import NewNote from './NewNote'
 import { useLocalStorage } from './useLocalStorage';
+import { v4 as uuid } from 'uuid';
 
 export type Note = {
   id: string
@@ -10,12 +12,11 @@ export type Note = {
 
 export type RawNote = {
   id: string
-}
+} & RawNoteData
 
 export type RawNoteData = {
   title: string,
   markdown: string,
-  tags: Tag[],
   tagIds: string[]
 }
 
@@ -35,12 +36,27 @@ function App() {
   const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
   const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
 
+  const notesWithTags = useMemo(() => {
+    return notes.map(note => {
+      return { ...note, tags: tags.filter(tag => note.tagIds.includes(tag.id)) }
+    })
+  }, [notes, tags])
+
+  function onCreateNote({ tags, ...data }: NoteData) {
+    setNotes(prevNotes => {
+      return [
+        ...prevNotes,
+        { ...data, id: uuid(), tagIds: tags.map(tag => tag.id) },
+      ]
+    })
+  }
+
   return (
     <Container className='my-4'>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<h1>Home</h1>} />
-          <Route path="/new" element={<NewNote />} />
+          <Route path="/new" element={<NewNote onSubmit={onCreateNote} />} />
           <Route path='/:id'>
             <Route index element={<h1>Show</h1>} />
             <Route path='edit' element={<h1>Edit</h1>} />
--- client/src/NewNote.tsx
+++ client/src/NewNote.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { NoteData } from './App';
 import NoteForm from './Components/NoteForm';
 
-const NewNote = () => {
+type NewNoteProps = {
+  onSubmit: (data: NoteData) => void
+}
+
+const NewNote = ({ onSubmit }: NewNoteProps) => {
   return (
     <>
       <h1 className='mb-4'>New Notes</h1>
-      <NoteForm />
+      <NoteForm onSubmit={onSubmit} />
     </>
   )
 }
